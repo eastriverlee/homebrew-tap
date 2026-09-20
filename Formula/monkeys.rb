ddc89e871f2d1b62b1d3b53628539d0c1d8d4fa1
eastriver <lee@eastriver.dev>
2026-09-21 06:37:12 +0900

    Revert "monkeys 0.13.1"
    This reverts commit 7b3579895d47cf5b90256247406625a17b562d3d.

--- Formula/monkeys.rb
+++ Formula/monkeys.rb
@@ -1,22 +1,22 @@
 class Monkeys < Formula
   desc ".env you can hand to an LLM, or git add"
   homepage "https://github.com/eastriverlee/monkeys"
-  version "0.13.1"
+  version "0.13.0"
   license "MIT"
 
   on_macos do
-    url "https://github.com/eastriverlee/monkeys/releases/download/v0.13.1/monkeys-macos-universal.tar.gz"
-    sha256 "ed77e28ed0b304ce3add2a4eda1612359e12fa4974b3b8cc2b9a703a3ee82afa"
+    url "https://github.com/eastriverlee/monkeys/releases/download/v0.13.0/monkeys-macos-universal.tar.gz"
+    sha256 "f4b428acbb9167964569b77fdd2c9fef6055465234495d051eba6f3e5bb36ded"
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/eastriverlee/monkeys/releases/download/v0.13.1/monkeys-linux-arm64.tar.gz"
-      sha256 "a58a4a294dee55acabf733bf6e0a7872816cb8a90c9c681aa9e981e93251c650"
+      url "https://github.com/eastriverlee/monkeys/releases/download/v0.13.0/monkeys-linux-arm64.tar.gz"
+      sha256 "4ddcf95f162a0f9588cc3db4b8cbb6df4b76c5d4930f09e4f719120ba3543e0f"
     end
     on_intel do
-      url "https://github.com/eastriverlee/monkeys/releases/download/v0.13.1/monkeys-linux-x86_64.tar.gz"
-      sha256 "2aceea741301b590a0d7560f4e6f473785252af5f8af38665f13bfc73cb7d016"
+      url "https://github.com/eastriverlee/monkeys/releases/download/v0.13.0/monkeys-linux-x86_64.tar.gz"
+      sha256 "763001f5f0d3d6716f011e76ca77ffa3ff4aed398ec9f14ef10b839b2c532014"
     end
   end
 
